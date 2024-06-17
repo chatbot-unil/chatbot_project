b0059537e350c60a04cd620d4a0c051ae034f4f4
DenisGremaud <32323238+DenisGremaud@users.noreply.github.com>
2024-06-17 13:34:33 +0200

    update rules for backend & frontend .env conf

--- terraform/main.tf
+++ terraform/main.tf
@@ -56,6 +56,26 @@ resource "openstack_networking_secgroup_rule_v2" "nginx_rule_egress" {
   security_group_id = openstack_networking_secgroup_v2.chatbot_security_group.id
 }
 
+resource "openstack_networking_secgroup_rule_v2" "backend_rule_ingress" {
+  direction         = "ingress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 3001
+  port_range_max    = 3001
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = openstack_networking_secgroup_v2.chatbot_security_group.id
+}
+
+resource "openstack_networking_secgroup_rule_v2" "backend_rule_egress" {
+  direction         = "egress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 3001
+  port_range_max    = 3001
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = openstack_networking_secgroup_v2.chatbot_security_group.id
+}
+
 resource "openstack_networking_secgroup_rule_v2" "ssh_rule" {
   direction         = "ingress"
   ethertype         = "IPv4"
