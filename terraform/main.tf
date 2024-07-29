696e0d33c378b3a5433e8313272bc5ffb86a3d3a
DenisGremaud <32323238+DenisGremaud@users.noreply.github.com>
2024-07-29 13:48:43 +0200

    update terraform

--- terraform/main.tf
+++ terraform/main.tf
@@ -56,6 +56,26 @@ resource "openstack_networking_secgroup_rule_v2" "nginx_rule_egress" {
   security_group_id = openstack_networking_secgroup_v2.chatbot_security_group.id
 }
 
+resource "openstack_networking_secgroup_rule_v2" "nginx_ssl_rule_ingress" {
+  direction         = "egress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 443
+  port_range_max    = 443
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = openstack_networking_secgroup_v2.chatbot_security_group.id
+}
+
+resource "openstack_networking_secgroup_rule_v2" "nginx_ssl_rule_egress" {
+  direction         = "egress"
+  ethertype         = "IPv4"
+  protocol          = "tcp"
+  port_range_min    = 443
+  port_range_max    = 443
+  remote_ip_prefix  = "0.0.0.0/0"
+  security_group_id = openstack_networking_secgroup_v2.chatbot_security_group.id
+}
+
 resource "openstack_networking_secgroup_rule_v2" "postgres_rule_egress" {
   direction         = "egress"
   ethertype         = "IPv4"
